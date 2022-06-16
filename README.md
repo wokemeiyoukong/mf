66d57f6dfe2996c23cb9b080fd18cdc4d8317310
fangmj <13085510025@163.com>
2022-06-16 16:49:56 +0800

    feat refactor code style and add file create ability

---
+++ README.md
@@ -0,0 +1,12 @@
+### `fmj-cli`
+
+```txt
+  基础的模版选择安装功能
+    模板组织： 'https://api.github.com/orgs/little-cli/repos'
+  文件快速生成功能
+    日常的git提交shell脚本快速生成 避免复制拷贝
+
+  afu --help
+  afu create <PROJECT_NAME>
+  afu commit <SHELL_NAME> -d <destination>
+```
--- bin/cli.js
+++ bin/cli.js
@@ -1,52 +1,19 @@
 #! /usr/bin/env node
 
-console.log('🇨🇳  fmj-cli is working~ \n')
-
 const program = require('commander')
 const chalk = require('chalk')
 const figlet = require('figlet')
 
-program
-  .command('create <app-name>')
-  .description('Create a new project')
-  //
-  .option('-f, --force', 'overwrite existing project if it already exists')
-  .action((name, option) => {
-    // console.log(`name:${name}`, option)
-    require('../lib/create')(name, option)
-  })
+const optionsCreate = require('../lib/command/options')
+const createCommand = require('../lib/command/create')
 
-// 配置 config 命令
-// program
-//   .command('config [value]')
-//   .description('inspect and modify the config')
-//   .option('-g, --get <path>', 'get value from option')
-//   .option('-s, --set <path> <value>')
-//   .option('-d, --delete <path>', 'delete option from config')
-//   .action((value, options) => {
-//     console.log(value, options)
-//   })
+console.log('afu is working~ \n')
 
-program
-  .version(`v${require('../package.json').version}`)
-  .usage('<command> [option]')
+program.version(`v${require('../package.json').version}`)
 
-program.on('--help', () => {
-  // figlet for logo
-  console.log(
-    '\r\n' +
-      figlet.textSync('fang', {
-        font: 'Ghost',
-        horizontalLayout: 'default',
-        verticalLayout: 'default',
-        width: 80,
-        whitespaceBreak: true
-      })
-  )
-  console.log(
-    `\r\nRun ${chalk.cyan(`fmj-cli <command> --help`)} for details\r\n`
-  )
-})
+optionsCreate()
+
+createCommand()
 
 // parse shell command args
 program.parse(process.argv)
